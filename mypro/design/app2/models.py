a66a6ea2fa757ff5adf5ffce950146de6deca23d
ragikrishnavk123 <ragikrishna77@gmail.com>
2024-02-06 11:40:02 +0530

    new added

---
+++ mypro/design/app2/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Place(models.Model):
+    name=models.CharField(max_length=20)
+    desc=models.TextField()
+    image=models.ImageField(upload_to="image/places",null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Team(models.Model):
+    name=models.CharField(max_length=20)
+    desc=models.TextField()
+    image=models.ImageField(upload_to="image/team",null=True,blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ mypro/design/app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from app2.models import Place,Team
+def home(request):
+    p=Place.objects.all()
+    t=Team.objects.all()
+    return render(request,'home.html',{'p':p,'t':t})
+# Create your views here.
